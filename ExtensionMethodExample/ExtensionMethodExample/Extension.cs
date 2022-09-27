901201d8975d3fbc2a691517beb97d181e850776
imran5740 <imranbsmrstucse@gmail.com>
2022-09-27 14:37:12 +0600

    New File Added

---
+++ ExtensionMethodExample/ExtensionMethodExample/Extension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodExample
+{
+    static class Extension
+    {
+        public static string GetInfo(this Person person)
+        {
+            return "Person ID :" + person.Id + Environment.NewLine+ "Person Name :" + person.Name + Environment.NewLine + "Person Address :" + person.Address + Environment.NewLine;
+        }
+    }
+}
---
+++ ExtensionMethodExample/ExtensionMethodExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethodExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person person = new Person();
+            person.Id = 101;
+            person.Name = "Mohammad Imran";
+            person.Address = "Satana Baluya,Gobindaganj,Gaibandha";
+            string info=person.GetInfo();
+            Console.WriteLine(info);
+            Console.ReadKey();
+        }
+    }
+}
