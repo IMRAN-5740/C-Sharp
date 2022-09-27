62b5ff69f77cfb0eb4cfadd55f6de445c879caae
imran5740 <imranbsmrstucse@gmail.com>
2022-09-27 19:42:49 +0600

    ExtionMethodFile Added

---
+++ ReflectionExample/ReflectionExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReflectionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student = new Student();
+            var studentInfo = typeof(Student);
+            string assemblyName=studentInfo.Assembly.FullName;
+            Console.WriteLine(assemblyName);
+            var methods=studentInfo.GetMethods();
+            //foreach (var method in methods)
+            //{
+            //    Console.WriteLine("Return Type :"+method.ReturnType+"Name :"+method.Name);
+            //}
+            var Properties=studentInfo.GetProperties();
+            foreach (var property in Properties)
+            {
+                Console.WriteLine(property.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReflectionExample/ReflectionExample/Student.cs
@@ -0,0 +1,21 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionExample
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public string RegNo { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string GetInfo()
+        {
+            return "Student Id :" + Id + Environment.NewLine + "Student Name :" + Name + Environment.NewLine +"Student Age :"+Age+Environment.NewLine+"Student Registraion No :" + RegNo + Environment.NewLine + "Student Address :" + Address + Environment.NewLine;
+        }
+    }
+    
+}
